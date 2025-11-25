de74f4ea06fe24a6c23e64a03800b8d1dc20fbe3
Sowntharya <139870927+sownd18@users.noreply.github.com>
2025-11-25 09:35:09 +0530

    Add files via upload

---
+++ Assignment-3/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import DiceChallenge from './components/DiceChallenge'
+
+
+function App() {
+  return (
+    <>
+    <DiceChallenge/>
+    </>
+  )
+}
+
+export default App
