a144128d7b076b72848d4190dff8c7ce0fc476a4
Sowntharya <139870927+sownd18@users.noreply.github.com>
2025-12-02 18:51:01 +0530

    Delete Assignment-5 directory

--- Assignment-5/middleware/customMiddleware.js
+++
@@ -1,8 +0,0 @@
-const customMiddleware =(req,res,next)=>{
-    let verify=true;
-    if(!verify){
-        return res.send("User Verification  not found");
-    }
-    next();
-};
-module.exports=customMiddleware;
--- Assignment-5/model/userSchema.js
+++
@@ -1,8 +0,0 @@
-const mongoose=require("mongoose");
-
-const newSchema=new mongoose.Schema({
-    name:String,
-    email:String,
-    password:String
-})
-module.exports=mongoose.model("USER",newSchema)
