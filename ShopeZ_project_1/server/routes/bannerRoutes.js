18867d1bf4284d9c047470eb5269db6a6c5105a5
Sowntharya <139870927+sownd18@users.noreply.github.com>
2025-12-02 18:51:19 +0530

    Delete ShopeZ_project_1 directory

--- ShopeZ_project_1/server/routes/bannerRoutes.js
+++
@@ -1,9 +0,0 @@
-// backend/routes/bannerRoutes.js
-import express from 'express';
-import { getAllBanners } from '../controllers/bannerController.js';
-const router = express.Router();
-
-// Public route
-router.get('/', getAllBanners);
-
-export default router;
